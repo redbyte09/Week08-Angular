281656018ada07baf7550147d9ea56bf2c5df733
redbyte09 <sakshisharma4234@gmail.com>
2025-03-06 10:42:19 +0530

    [Sakshi] Add .Display hello bridgelabz with user input

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   message: string = "Hello from BridgeLabz";  
   logoPath: string = "./assets/BL_logo_square_jpg.jpg";   
-
+  userName: string = "";
   openBridgeLabz() {
     window.open("https://www.bridgelabz.com", "_blank");
   }
