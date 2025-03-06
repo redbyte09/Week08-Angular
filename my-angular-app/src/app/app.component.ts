f021c656d21de1ed5074d359d77b6753137c93cd
redbyte09 <sakshisharma4234@gmail.com>
2025-03-06 10:24:27 +0530

    [Sakshi] Add .refactored angular.json for assets

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+  message: string = "Hello from BridgeLabz";  // Define message property
+  logoPath: string = "./assets/BL_logo_square_jpg.jpg"; 
+
 }
